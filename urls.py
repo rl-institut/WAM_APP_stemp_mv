7113313d114603f572026ba3c85e94b40a2c2566
hendrik <hendrik.huyskens@rl-institut.de>
2018-04-11 14:22:33 +0200

    Refactored demand into separate views for single and district

--- urls.py
+++ urls.py
@@ -2,8 +2,9 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 from stemp.views import (
-    SelectView, ResultView, ParameterView, ComparisonView, DemandView,
-    TechnologyView, DemandSelectionView
+    SelectView, ResultView, ParameterView, ComparisonView, DemandSingleView,
+    DemandDistrictView, TechnologyView,
+    DemandSelectionView
 )
 from stemp.views_dynamic import HouseholdProfileView
 
@@ -21,7 +22,17 @@ urlpatterns = [
         DemandSelectionView.as_view(),
         name='demand_selection'
     ),
-    path('demand/<str:selection>/', DemandView.as_view(), name='demand'),
+    path('demand/single/', DemandSingleView.as_view(), name='demand_single'),
+    path(
+        'demand/district/household/: ',
+        DemandSingleView.as_view(is_district_hh=True),
+        name='demand_district_household'
+    ),
+    path(
+        'demand/district/',
+        DemandDistrictView.as_view(),
+        name='demand_district'
+    ),
     path('technology/', TechnologyView.as_view(), name='technology'),
     path('parameter/', ParameterView.as_view(), name='parameter'),
     path('result/', ResultView.as_view(), name='result'),
--- views.py
+++ views.py
@@ -1,5 +1,4 @@
 
-from collections import namedtuple, OrderedDict
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
 
@@ -15,26 +14,6 @@ from .forms import (
 )
 from stemp.results import Comparison
 from stemp.scenarios import get_scenario_config
-
-Option = namedtuple('Option', ['label', 'value', 'image'])
-demand_options = OrderedDict(
-    [
-        (
-            'structure',
-            [
-                Option('Einzelhaushalt', 'single', '<img>'),
-                Option('Quartier', 'district', '<img>')
-            ]
-        ),
-        (
-            'selection',
-            [
-                Option('Aus einer Liste wählen', 'list', '<img>'),
-                Option('Per Fragen', 'questions', '<img>'),
-            ]
-        )
-    ]
-)
 
 
 def check_session(func):
@@ -55,65 +34,63 @@ class DemandSelectionView(TemplateView):
     template_name = 'stemp/demand_selection.html'
 
 
-class DemandView(TemplateView):
-    template_name = 'stemp/demand.html'
+class DemandSingleView(TemplateView):
+    template_name = 'stemp/demand_single.html'
+    is_district_hh = False
 
     def __init__(self, **kwargs):
-        super(DemandView, self).__init__(**kwargs)
+        super(DemandSingleView, self).__init__(**kwargs)
 
-    @staticmethod
-    def __build_hh_selection(context, structure, selection, hh_form=None):
-        if selection is None or selection in ('add_household', 'back'):
-            context['options']['selection'] = demand_options['selection']
-        elif selection == 'list':
-            context['options']['selection'] = [demand_options['selection'][0]]
-            context['selection_form'] = HouseholdSelectForm()
-            context['demand_submit_value'] = structure + '_list'
-        elif selection == 'questions':
-            context['options']['selection'] = [demand_options['selection'][1]]
-            context['selection_form'] = HouseholdQuestionsForm()
-            context['demand_submit_value'] = structure + '_questions'
-        elif selection == 'new':
-            context['options']['selection'] = [demand_options['selection'][1]]
-            context['selection_form'] = hh_form
-            context['demand_submit_value'] = structure + '_new'
+    def get_context_data(self, hh_proposal=None):
+        context = super(DemandSingleView, self).get_context_data()
+        context['specific_form'] = HouseholdQuestionsForm()
+        context['list_form'] = HouseholdSelectForm()
+        context['new_form'] = hh_proposal
+        return context
+
+    def get(self, request, *args, **kwargs):
+        # Start session (if no session yet):
+        SESSION_DATA.start_session(request)
+
+        context = self.get_context_data()
+        return self.render_to_response(context)
+
+    @check_session
+    def post(self, request, session):
+        hh_id = None
+        if 'questions' in request.POST:
+            hh_questions = HouseholdQuestionsForm(request.POST)
+            if hh_questions.is_valid():
+                proposal_form = hh_questions.hh_proposal()
+                context = self.get_context_data(proposal_form)
+                return self.render_to_response(context)
+        elif 'new' in request.POST:
+            hh_form = HouseholdForm(request.POST)
+            if hh_form.is_valid():
+                hh = hh_form.save()
+                hh_id = hh.id
+        elif 'list' in request.POST:
+            hh = HouseholdSelectForm(request.POST)
+            if hh.is_valid():
+                hh_id = hh.cleaned_data['profile'].id
+
+        if self.is_district_hh:
+            session.district[str(hh_id)] = 1
+            return redirect('stemp:demand_district')
         else:
-            raise ValueError(
-                'Unknown demand selection "' + str(selection) +
-                '" for structure "' + structure + "'")
+            session.parameter = {
+                'customer_index': hh_id,
+                'customer_case': 'single'
+            }
+            return redirect('stemp:technology')
 
-    def __get_single_context(self, context, data):
-        context['options']['structure'] = [demand_options['structure'][0]]
-        selection = data.get('selection')
-        self.__build_hh_selection(
-            context, 'single', selection, data.get('hh_proposal'))
 
-    def __get_district_context(self, session, context, data):
-        context['options']['structure'] = [demand_options['structure'][1]]
-        selection = data.get('selection')
-        if selection is None:
-            context['selection_form'] = DistrictListForm(session.district)
-            context['demand_submit_value'] = 'district_done'
-            return
-        self.__build_hh_selection(
-            context, 'district', selection, data.get('hh_proposal'))
-        return
+class DemandDistrictView(TemplateView):
+    template_name = 'stemp/demand_district.html'
 
-    def get_context_data(self, session, data):
-        context = super(DemandView, self).get_context_data()
-        context['options'] = OrderedDict()
-
-        structure = data.get('structure')
-        if structure is None or structure == 'back':
-            # First choose between single and district:
-            context['options']['structure'] = demand_options['structure']
-        elif structure == 'single':
-            self.__get_single_context(context, data)
-        elif structure == 'district':
-            self.__get_district_context(session, context, data)
-        else:
-            raise ValueError(
-                'Unknown demand structure "' + str(structure) + '"')
+    def get_context_data(self, session):
+        context = super(DemandDistrictView, self).get_context_data()
+        context['district_form'] = DistrictListForm(session.district)
         return context
 
     def get(self, request, *args, **kwargs):
@@ -121,8 +98,7 @@ class DemandView(TemplateView):
         SESSION_DATA.start_session(request)
         session = SESSION_DATA.get_session(request)
 
-        data = request.GET
-        context = self.get_context_data(session, data)
+        context = self.get_context_data(session)
         return self.render_to_response(context)
 
     def __change_district_list(self, request, session):
@@ -139,72 +115,18 @@ class DemandView(TemplateView):
         if 'trash' in request.POST:
             trash = request.POST['trash']
             del session.district[trash]
-            data = {'structure': 'district'}
-            context = self.get_context_data(session, data)
+            context = self.get_context_data(session)
             return self.render_to_response(context)
         elif 'add_household' in request.POST:
-            data = {
-                'structure': 'district',
-                'selection': 'add_household',
-            }
-            context = self.get_context_data(session, data)
-            return self.render_to_response(context)
+            return redirect('stemp:demand_district_household')
 
     @check_session
-    def post(self, request, session, selection):
-        if 'done' not in request.POST:
-            return self.__change_district_list(request, session)
-        submit = request.POST.get('demand_submit')
-        if submit is None:
-            raise ValueError('Invalid demand post!')
-        if submit in ('single_questions', 'district_questions'):
-            hh_questions = HouseholdQuestionsForm(request.POST)
-            if hh_questions.is_valid():  # TODO: If not valid...
-                data = {
-                    'structure': request.POST['demand_submit'].split('_')[0],
-                    'selection': 'new',
-                    'hh_proposal': hh_questions.hh_proposal()
-                }
-                context = self.get_context_data(session, data)
-                return self.render_to_response(context)
-        if submit in ('single_new', 'district_new'):
-            hh_form = HouseholdForm(request.POST)
-            if hh_form.is_valid():  # TODO: If not valid...
-                hh = hh_form.save()
-                if submit.split('_')[0] == 'single':
-                    session.parameter = {
-                        'customer_index': hh.id,
-                        'customer_case': 'single'
-                    }
-                    return redirect('stemp:technology')
-                elif submit.split('_')[0] == 'district':
-                    session.district[str(hh.id)] = 1
-                    data = {'structure': 'district'}
-                    context = self.get_context_data(session, data)
-                    return self.render_to_response(context)
-                else:
-                    raise ValueError('Unknown submit key "' + submit)
-        if submit in ('single_list', 'district_list'):
-            hh = HouseholdSelectForm(request.POST)
-            if hh.is_valid():  # TODO: If not valid...
-                hh_id = hh.cleaned_data['profile'].id
-            if submit.split('_')[0] == 'single':
-                session.parameter = {
-                    'customer_index': hh_id,
-                    'customer_case': 'single'
-                }
-                return redirect('stemp:technology')
-            elif submit.split('_')[0] == 'district':
-                session.district[str(hh_id)] = 1
-                data = {'structure': 'district'}
-                context = self.get_context_data(session, data)
-                return self.render_to_response(context)
-            else:
-                raise ValueError('Unknown submit key "' + submit)
-        else:
-            # TODO: Correct parameter input of district:
+    def post(self, request, session):
+        if 'done' in request.POST:
             session.parameter = session.district
             return redirect('stemp:technology')
+        else:
+            return self.__change_district_list(request, session)
 
 
 class TechnologyView(TemplateView):
@@ -323,7 +245,7 @@ class ResultView(TemplateView):
         return self.render_to_response(context)
 
     @check_session
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         if 'save' in request.POST:
             simulation_name = request.POST['simulation_name']
             try:
