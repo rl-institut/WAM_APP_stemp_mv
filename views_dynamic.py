ae4362bc2310da00297e033ff89df5f66e555bfe
hendrik <hendrik.huyskens@rl-institut.de>
2018-11-08 13:18:20 +0100

    Removed legacy dynamic views

--- views_dynamic.py
+++ views_dynamic.py
@@ -1,11 +1,9 @@
 
 from django.http import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.views.generic import TemplateView
 
 from stemp import constants
-from stemp.forms import HouseholdSelectForm
-from stemp.models import District, Household
+from stemp.models import Household
 
 
 def get_next_household_name(request):
@@ -45,44 +43,3 @@ def get_energy(request):
         raise ValueError(f'Unknown choice "{choice}". Cannot calculate energy')
     energy = sm * constants.ENERGY_PER_QM_PER_YEAR[house_type]
     return JsonResponse({'energy': energy})
-
-
-class HouseholdProfileView(TemplateView):
-    template_name = 'stemp/load_profile.html'
-
-    def get_context_data(self, household=None):
-        if household is None:
-            return {}
-        else:
-            hh = Household.objects.get(id=household)
-            hc = hh.as_highchart()
-            context = {
-                'load_graph': hc.render('load_graph')
-            }
-            return context
-
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(int(
-            request.GET.get('choice', 1)))
-        return self.render_to_response(context)
-
-
-class SwitchLoadView(TemplateView):
-    template_name = 'stemp/load_switch.html'
-
-    def get_context_data(self, switch, **kwargs):
-        context = super(SwitchLoadView, self).get_context_data(**kwargs)
-        if switch == 0:
-            context['is_graph'] = False
-            context['load'] = HouseholdSelectForm()
-        else:
-            context['is_graph'] = True
-            dist = District.objects.get(id=1)
-            hc = dist.as_highchart()
-            context['load'] = hc.render('load_graph')
-        return context
-
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(int(
-            request.GET.get('choice', 0)))
-        return self.render_to_response(context)
