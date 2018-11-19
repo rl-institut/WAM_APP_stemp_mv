628877d1954d0ab076afc25244b6f44a0126393d
hendrik <hendrik.huyskens@rl-institut.de>
2018-11-19 17:26:17 +0100

    Fixed minor bug

--- views.py
+++ views.py
@@ -189,9 +189,10 @@ class TechnologyView(TemplateView):
             if 'description' in params['LABELS']
         }
 
+        demand = session.get_demand()
+
         # Add warning if radiator is chosen in combination with heatpump:
         if 'pv_heatpump' in app_settings.ACTIVATED_SCENARIOS:
-            demand = session.get_demand()
             if demand.contains_radiator():
                 technology_information['pv_heatpump']['warning'] = (
                     app_settings.SCENARIO_PARAMETERS[
