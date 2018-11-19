da49299cea91d8869917147da4dca441d2e938f6
hendrik <hendrik.huyskens@rl-institut.de>
2018-11-19 12:14:50 +0100

    Adapted new admin site

--- admin.py
+++ admin.py
@@ -1,13 +1,14 @@
-from django.contrib import admin
+
+from wam.admin import wam_admin_site
 from stemp import models
 
 # Register load db:
-admin.site.register(models.District)
-admin.site.register(models.Household)
-admin.site.register(models.DistrictHouseholds)
-admin.site.register(models.HeatProfile)
+wam_admin_site.register(models.District)
+wam_admin_site.register(models.Household)
+wam_admin_site.register(models.DistrictHouseholds)
+wam_admin_site.register(models.HeatProfile)
 
 # Register simulations
-admin.site.register(models.Scenario)
-admin.site.register(models.Parameter)
-admin.site.register(models.Simulation)
+wam_admin_site.register(models.Scenario)
+wam_admin_site.register(models.Parameter)
+wam_admin_site.register(models.Simulation)
