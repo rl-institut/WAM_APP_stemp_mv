afd0e2ec147a1d154feb2d185166f7f56e9c3b84
hendrik <hendrik.huyskens@rl-institut.de>
2019-01-07 08:58:25 +0100

    user session accessible for all apps

--- apps.py
+++ apps.py
@@ -1,16 +1,6 @@
 
 from django.apps import AppConfig
 
-from wam import settings
-from stemp import app_settings
-
 
 class StempConfig(AppConfig):
     name = 'stemp'
-
-    def ready(self):
-        """This function is executed right after project settings"""
-
-        # Initialize stemp sessions:
-        from wam.sessions import SessionData
-        settings.SESSION_DATA = SessionData()
