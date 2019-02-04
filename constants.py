ad962807c7ea3dfc7189e2c462316a431435bf37
hendrik <hendrik.huyskens@rl-institut.de>
2019-02-04 12:53:37 +0100

    Removed unnecessary LoadProfile class

--- constants.py
+++ constants.py
@@ -1,6 +1,5 @@
 
-import pandas
-from random import uniform
+from collections import OrderedDict
 from enum import Enum, IntEnum
 
 TIME_INDEX = [str(i + 1) + 'h' for i in range(24)]
@@ -60,37 +59,6 @@ class DistrictStatus(Enum):
     New = 'new'
     Changed = 'changed'
     Unchanged = 'unchanged'
-
-
-class LoadProfile(Enum):
-    Standard = pandas.Series(
-        [uniform(0, 30) for i in range(24)],
-        index=TIME_INDEX
-    )
-    HighLoad = pandas.Series(
-        [uniform(0, 30) for i in range(24)],
-        index=TIME_INDEX
-    )
-
-    def __init__(self, profile):
-        self.profile = profile
-
-    def __new__(cls, profile):
-        value = len(cls.__members__)
-        obj = object.__new__(cls)
-        obj._value_ = value
-        return obj
-
-    def __eq__(self, other):
-        return self.value == other
-
-    @classmethod
-    def get_profile(cls, index):
-        assert isinstance(index, int)
-        for member in cls.__members__.values():
-            if member.value == index:
-                return member.profile
-        raise IndexError('Index "' + str(index) + '" not found in LoadProfile')
 
 
 def get_roof_square_meters(household_square_meters, house_type):
