23bd666a20996fef1e2d35a1976d4a1e661d15ea
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2019-12-13 14:04:11 +0100

    Docstrings for models.py

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
+
 import pandas
 import sqlahelper
 import transaction
+
 from django.utils import timezone
 from django.db import models
-from django.utils.safestring import mark_safe
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.postgres.fields import ArrayField, JSONField
 
@@ -12,6 +13,7 @@ from stemp import oep_models
 
 
 class Parameter(models.Model):
+    """Holds all parameters for a simulation run"""
     data = JSONField(unique=True)
 
     def __str__(self):
@@ -19,6 +21,7 @@ class Parameter(models.Model):
 
 
 class Scenario(models.Model):
+    """Holds name and date of simulation run"""
     name = models.CharField(max_length=255, unique=True)
     last_change = models.DateTimeField(default=timezone.now)
 
@@ -27,6 +30,11 @@ class Scenario(models.Model):
 
 
 class Simulation(models.Model):
+    """Connects szenario with parameters to oemof results.
+
+    As oemof results are stored via sqlalchemy, relation between simulation and result
+    is built via "result_id" (*not* Foreign-key, thus loose coupled).
+    """
     scenario = models.ForeignKey(Scenario, on_delete=models.CASCADE)
     parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE)
     result_id = models.IntegerField()
@@ -38,6 +46,7 @@ class Simulation(models.Model):
 
     @classmethod
     def delete_containing_household(cls, hh_id):
+        """Deletes all simulations which contain given household ID"""
         districts = Household.objects.get(pk=hh_id).district_set.all()
         for district in districts:
             simulations = cls.objects.filter(
@@ -59,6 +68,7 @@ class Simulation(models.Model):
 
 
 class HeatProfile(models.Model):
+    """Model to hold heat profiles for different households and number of persons"""
     name = models.CharField(max_length=255)
     profile = ArrayField(models.FloatField(), size=8760, null=True)
 
@@ -73,12 +83,30 @@ class HeatProfile(models.Model):
 
 
 class DistrictHouseholds(models.Model):
+    """
+    Relation between districts and related households (many-to-many)
+
+    Amount is given to provide possibility to add multiple households of same type as
+    one entry.
+    """
     district = models.ForeignKey("District", on_delete=models.CASCADE)
     household = models.ForeignKey("Household", on_delete=models.CASCADE)
     amount = models.IntegerField()
 
 
 class Household(models.Model):
+    """
+    Household model contains all data to a single household
+
+    name: Name of the household
+    house_type: EFH (Einfamilienhaus) or MFH (Mehrfamilienhaus)
+    heat_demand: Total annual heat demand
+    number_of_persons: Persons belonging to the household
+    square_meters: Square meters of household
+    heat_type: radiator or floor
+    warm_water_per_day: Daily warm water consumption
+    roof_area: Potential photovoltaik area on roof of household
+    """
     timeseries = None
 
     name = models.CharField(
@@ -148,6 +176,15 @@ class Household(models.Model):
     )
 
     def get_oep_timeseries(self, name):
+        """
+        Function to get household timeseries
+
+        This function allows one time DB access, in order to avoid multiple database
+        look-ups.
+
+        Returns:
+            pandas.Series: Heat demand profile for given house type
+        """
         if self.timeseries is None:
             session = sqlahelper.get_session()
             with transaction.manager:
@@ -163,10 +200,23 @@ class Household(models.Model):
         return self.timeseries[name]
 
     def get_heat_demand_profile(self):
+        """
+        Get heat demand profile depending on given house type (EFH/MFH)
+
+        Returns:
+            pandas.Series: Heat demand profile for given house type
+        """
         house_type = constants.HouseType[self.house_type]
         return self.get_oep_timeseries(house_type.value)
 
     def annual_hot_water_demand(self):
+        """Get annual hot water demand profile
+
+        Depending on warmwater consumption per day and number of persons in household.
+
+        Returns:
+            pandas.Series: Annual hot water profile
+        """
         session = sqlahelper.get_session()
         with transaction.manager:
             hot_water = (
@@ -201,10 +251,14 @@ class Household(models.Model):
 
     @property
     def max_pv_size(self):
+        """
+        Photovoltaik potential for given roof area and PV-per-square-meters-ratio
+        """
         return self.roof_area / constants.QM_PER_PV_KW
 
 
 class District(models.Model):
+    """Multiple households combined to a district"""
     name = models.CharField(max_length=22)
     households = models.ManyToManyField("Household", through="DistrictHouseholds")
 
@@ -212,12 +266,23 @@ class District(models.Model):
         return self.name
 
     def add_households(self, households):
+        """
+        Adds multiple households to the district
+
+        Parameters:
+            households: dict[int, int]
+                Dictionary containing household-id as key and amount of this household
+                type as value
+        """
         for hh_id, amount in households.items():
             hh = Household.objects.get(pk=hh_id)
             district_hh = DistrictHouseholds(district=self, household=hh, amount=amount)
             district_hh.save()
 
     def annual_total_demand(self):
+        """
+        Returns combined annual totol demand (heat and warmwater) for all related
+        households in district"""
         return sum(
             [
                 dh.household.annual_total_demand() * dh.amount
@@ -226,6 +291,7 @@ class District(models.Model):
         )
 
     def annual_heat_demand(self):
+        """Returns combined annual heat demand for all households in district"""
         return sum(
             [
                 dh.household.annual_heat_demand() * dh.amount
@@ -234,6 +300,7 @@ class District(models.Model):
         )
 
     def annual_hot_water_demand(self):
+        """Returns combined annual hot water demand for all households in district"""
         return sum(
             [
                 dh.household.annual_hot_water_demand() * dh.amount
@@ -242,6 +309,7 @@ class District(models.Model):
         )
 
     def contains_radiator(self):
+        """Returns `True` if any household in district contains a radiator"""
         return any(
             [
                 hh.heat_type == constants.HeatType.radiator.name
@@ -251,4 +319,5 @@ class District(models.Model):
 
     @property
     def max_pv_size(self):
+        """Returns summarized PV potential in district"""
         return sum([hh.max_pv_size for hh in self.households.all()])
