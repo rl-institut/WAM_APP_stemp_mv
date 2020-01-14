e832ea34c2fa487f4f219de38df2e13b9e202262
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2020-01-14 10:57:20 +0100

    Docstrings for views_dynamic.py

--- views_dynamic.py
+++ views_dynamic.py
@@ -1,3 +1,5 @@
+"""Functions to handle dynamic AJAX-requests"""
+
 from django.http import JsonResponse, HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -17,6 +19,11 @@ def check_pending(request, session):
 
 
 def get_next_household_name(request):
+    """
+    Dynamic household naming
+
+    If household name is already present in DB, a counting number prefix is added
+    """
     name = request.GET["hh_name"]
     i = 0
     while True:
@@ -29,12 +36,14 @@ def get_next_household_name(request):
 
 
 def get_square_meters(request):
+    """Calculates square meters related to number of persons"""
     persons = float(request.GET["persons"])
     sm = round(persons * constants.QM_PER_PERSON)
     return JsonResponse({"square_meters": sm})
 
 
 def get_warm_water_energy(request):
+    """Calculates warm water consumption related to number of persons"""
     persons = float(request.GET["persons"])
     warmwater_consumption = constants.WarmwaterConsumption(
         int(request.GET["warmwater_consumption"])
@@ -45,6 +54,7 @@ def get_warm_water_energy(request):
 
 
 def get_heat_demand(request):
+    """Calculates heat demand related to square meters and household type"""
     sm = float(request.GET["sm"])
     house_type = request.GET["house_type"]
     heat_demand = round(sm * constants.ENERGY_PER_QM_PER_YEAR[house_type])
@@ -52,6 +62,7 @@ def get_heat_demand(request):
 
 
 def get_roof_area(request):
+    """Calculates roof area related to square meters and household type"""
     sm = int(request.GET["sm"])
     house_type = constants.HouseType[request.GET["house_type"]]
     sm = round(constants.get_roof_square_meters(sm, house_type))
@@ -59,6 +70,7 @@ def get_roof_area(request):
 
 
 def get_household_summary(request):
+    """Provides household summary widget for given household ID"""
     hh_id = int(request.GET["hh_id"])
     return HttpResponse(
         HouseholdSummary(Household.objects.get(pk=hh_id), use_header=False)
